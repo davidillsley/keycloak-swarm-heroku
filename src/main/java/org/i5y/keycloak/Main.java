39b42ebd992872347c904c5eec1157d701cd1697
klausboeing <klausboeing@gmail.com>
2017-03-19 02:58:39 -0300

    Atualizado

--- src/main/java/org/i5y/keycloak/Main.java
+++ src/main/java/org/i5y/keycloak/Main.java
@@ -44,6 +44,7 @@ public class Main {
                     ds.driverName("postgresql");
                     ds.connectionUrl(databaseUrl.jdbcUrl());
                     ds.userName(databaseUrl.username());
+                    ds.password(databaseUrl.password());
                 });
 //
         container.fraction(datasourcesFraction);
